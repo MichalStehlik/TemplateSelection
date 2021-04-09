a3326742ad048221e100eb143fe87fae6c11e4da
Michal Stehlík <michal.stehlik@pslib.cz>
2021-04-09 13:06:35 +0200

    Add project files.

---
+++ TemplateSelection/Models/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateSelection.Models
+{
+    class Human
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ TemplateSelection/ViewModels/PersonTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TemplateSelection.ViewModels
+{
+    class PersonTemplateSelector: DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject dependency)
+        {
+            if (item is Models.Teacher)
+                return TeacherTemplate;
+            else if (item is Models.Student)
+                return StudentTemplate;
+            else
+                return HumanTemplate;
+        }
+
+        public DataTemplate TeacherTemplate { get; set; }
+        public DataTemplate StudentTemplate { get; set; }
+        public DataTemplate HumanTemplate { get; set; }
+    }
+}
